791b03b76e5dd6eeb044aa1fb34b1ed32bba2550
nerkosokolic <nerkosokolic@gmail.com>
2019-08-05 14:08:01 +1000

    ready for deployment

--- app_console.rb
+++ app_console.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 require_relative 'database_config'
 require_relative 'models/user'
 require_relative 'models/business_unit'
@@ -9,4 +9,4 @@ require_relative 'models/utilisation_log'
 require_relative 'models/revenue_type'
 
 
-binding.pry
+# binding.pry
